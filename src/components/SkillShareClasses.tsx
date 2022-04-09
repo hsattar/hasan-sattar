836733757e41273a38bdfffd9d1d3bb4ce55272d
Hasan Sattar <hasanraza1798@gmail.com>
2022-04-10 00:14:59 +0100

    added skillshare description

--- src/components/SkillShareClasses.tsx
+++ src/components/SkillShareClasses.tsx
@@ -5,17 +5,17 @@ export default function SkillShareClasses() {
     const classes: IClass[] = [{
         image: '/assets/finance-planner.jpg',
         link: "https://www.skillshare.com/classes/How-to-Create-a-Finance-Planner-Personal-Budgeting-in-Microsoft-Excel/2055873377",
-        title: 'Create A Finance Planner',
+        title: 'How To Create A Finance Planner',
         students: "138"
     }, {
         image: '/assets/food-planner.jpg',
         link: "https://www.skillshare.com/classes/How-To-Create-a-Random-Food-Meal-Planner-in-Microsoft-Excel/538955742",
-        title: 'Create A Meal Planner',
+        title: 'How To Create A Meal Planner',
         students: "43"
     }, {
         image: '/assets/payroll-calculator.jpg',
         link: "https://www.skillshare.com/classes/How-to-Create-a-Payroll-Calculator-in-Microsoft-Excel/648854705",
-        title: 'Create A Payroll Calculator',
+        title: 'How To Create A Payroll Calculator',
         students: "44"
     }]
 
