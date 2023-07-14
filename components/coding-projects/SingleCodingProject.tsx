d41748651ee2ea42c3ab4874263d25b3985ba96d
Hasan Sattar <hasanraza1798@gmail.com>
2023-07-14 23:38:37 +0100

    made small changes to projects page and update video link for code buddy

--- components/coding-projects/SingleCodingProject.tsx
+++ components/coding-projects/SingleCodingProject.tsx
@@ -11,17 +11,15 @@ interface IProps {
 export default function SingleCodingProject({ image, link, title, subtitle, description, technologies, direction }: IProps) {
     return (
         <section className={`flex flex-col items-center ${direction} p-4 select-none`}>
-            <div className="flex justify-center items-center w-full md:w-[50%]">
-                <a href={link} target="_blank" rel="noreferrer">
-                    <img src={image} className="w-full px-3 lg:w-[75%]" alt=""/>
-                </a>
-            </div>
+            <a href={link} target="_blank" rel="noreferrer" className="flex justify-center items-center w-full md:w-[50%]">
+                <img src={image} className="w-full px-3 lg:w-[75%]" alt=""/>
+            </a>
             <div className="flex flex-grow flex-col justify-between items-center text-center py-8">
                 <a href={link} target="_blank" rel="noreferrer">
                     <h4 className="text-center text-2xl font-medium">{title}</h4>
-                    <h5 className="mb-3">{subtitle}</h5>
+                    {/* <h5 className="mb-3">{subtitle}</h5> */}
                 </a>
-                <p className="mb-2">{description}</p>
+                <p className="mt-4 mb-2">{description}</p>
                 <p className="text-sm text-gray-600">{technologies}</p>
             </div>
         </section>
--- data/codingProjects.ts
+++ data/codingProjects.ts
@@ -3,15 +3,15 @@ const codingProjects: ICodingProjects[] = [{
     link: "https://smithsair.co.uk",
     title: 'Smiths Air',
     subtitle: "Solo Project",
-    description: "A workplace communication PWA.",
-    technologies: "Next.js, Redux, Tailwind CSS, Node.js, Express, MongoDB, TypeScript",
+    description: "A workplace communication PWA. Live Production App - 50 Users, ~ 20 DAU.",
+    technologies: "Next.js, Redux, Tailwind CSS, Node.js, Express, MongoDB, TypeScript, Jest",
     direction: "md:flex-row"
 }, {
     image: '/assets/code-buddy.jpg',
-    link: "https://www.youtube.com/watch?v=lpIls5bDdZE&t=4929s",
+    link: "https://youtu.be/gJ_Q4nzSVMs",
     title: 'Code Buddy',
     subtitle: "Solo Project",
-    description: "Code Buddy is a no-code platform that allows you to create websites and is simple to use.",
+    description: "Code Buddy is a no-code platform that allows you to create websites using a drag and drop interface.",
     technologies: "React, Redux, Tailwind CSS, Node.js, Express, MongoDB, TypeScript",
     direction: "md:flex-row-reverse"
 }]
