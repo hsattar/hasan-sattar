29714b1424b816e3ab90a31fe9805d3327789be7
Hasan Sattar <hasanraza1798@gmail.com>
2024-02-06 20:04:02 +0000

    removed a tag inside link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center px-4 py-3 bg-gray-100 select-none">
-            <Link href="/"><a className="text-4xl cursor-pointer">HS</a></Link>
+            <Link href="/" className="text-4xl cursor-pointer">HS</Link>
             {/* <div>
                 <Link href="/"><a className="mr-6">Home</a></Link>
                 <Link href="/projects"><a className="mr-6">Projects</a></Link>
