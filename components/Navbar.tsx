9f69bc73070122dfec4d9088f3afa9dbcc8eeeb1
Hasan Sattar <hasanraza1798@gmail.com>
2024-02-06 20:15:37 +0000

    added legacy behaviour to link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center px-4 py-3 bg-gray-100 select-none">
-            <Link href="/" className="text-4xl cursor-pointer">HS</Link>
+            <Link  legacyBehavior href="/"><a className="text-4xl cursor-pointer">HS</a></Link>
             {/* <div>
                 <Link href="/"><a className="mr-6">Home</a></Link>
                 <Link href="/projects"><a className="mr-6">Projects</a></Link>
